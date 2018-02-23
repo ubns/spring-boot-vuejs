f36f2e6b07116c484e453c88f7adf306d66a2425
Node4m <node4m@gmail.com>
2018-02-24 01:53:06 +0900

    Initial Project at 2018.02.23

---
+++ src/main/java/spa/SpaApplication.java
@@ -0,0 +1,12 @@
+package spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaApplication.class, args);
+	}
+}
