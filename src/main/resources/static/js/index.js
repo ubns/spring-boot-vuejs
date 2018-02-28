7b50c6ae6517227f461e75bcb207b4a79abd02eb
Node4m <node4m@gmail.com>
2018-03-01 00:47:33 +0900

    Change Vue Source at 2018.02.28

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,12 +1,19 @@
-// ローカル登録
-var Child = {
-    template: '<div>Spring boot child</div>'
-}
-
-// root インスタンスの生成
-new Vue({
-    el: '#example',
-    components: {
-        'my-component': Child
+var vm = new Vue({
+    el:'#app',
+    data: {
+        users: ''
+    },
+    methods: {
+        get: function () {
+            var self = this
+            axios.get("/api/user/1")
+                .then(function (res) {
+                    self.users = res.data
+                })
+        },
+        hoge: function () {
+            this.users = {id: 1, name: "hoge", password:"hogehoge"}
+        }
     }
 })
+
